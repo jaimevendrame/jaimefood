b9385716950c94584e4971b8e6637d40898ebe7c
Jaime Vendrame <jaimevendrame@MacBook-Pro-de-Jaime.local>
2021-05-10 23:23:22 -0300

    endcommit

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -17,7 +17,7 @@ class ProductRepository implements ProductRepositoryInterface
     {
         return DB::table($this->table)
                 ->join('category_product', 'category_product.product_id', '=', 'products.id')
-                ->join('categories', 'category_product.product_id', '=', 'categories.id')
+                ->join('categories', 'category_product.category_id', '=', 'categories.id')
                 ->where('products.tenant_id', $idTenant)
                 ->where('categories.tenant_id', $idTenant)
                 ->where(function ($query) use ($categories){
